2121c9912039722c8f8a4082d5ef727f0625511b
CuiBo2Coder <2447308975@qq.com>
2022-09-13 20:23:39 +0800

    1.0

---
+++ TestHelloWorld/src/com/atguigu/domain.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author CuiBo
+ * @create 2022-09-13 20:17
+ */
+public class domain {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
